4f4fe615731476addd584c3bd0fbb8560970a67e
erwan Leboucher <erwanleboucher@gmail.com>
2015-02-03 18:11:06 +0100

    Update and rename cm.mk to bliss.mk

--- bliss.mk
+++ bliss.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/samsung/v2wifixx/full_v2wifixx.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_v2wifixx
+PRODUCT_NAME := bliss_v2wifixx
 PRODUCT_DEVICE := v2wifixx
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
