aa82a328ac78ae3ea8f238f92ef00a9f317320f2
erwan Leboucher <erwanleboucher@gmail.com>
2015-02-05 16:25:13 +0100

    Update bliss.mk

--- bliss.mk
+++ bliss.mk
@@ -3,15 +3,11 @@ $(call inherit-product, device/samsung/v2wifixx/full_v2wifixx.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
+PRODUCT_NAME := bliss_v2wifixx
 PRODUCT_DEVICE := v2wifixx
-PRODUCT_NAME := bliss_v2wifixx 
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T900
-PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T900 \
     PRODUCT_NAME=v2wifixx \
     PRODUCT_DEVICE=v2wifixx \
     TARGET_DEVICE=v2wifixx \
-
