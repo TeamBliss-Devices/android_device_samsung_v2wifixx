fded11bf2cc89a7d7ea204f3854254d7af567875
nerdyblonde <krisnicbell@gmail.com>
2015-12-10 10:56:04 -0800

    Try to build Bliss for this big ass tablet

--- bliss.mk
+++ bliss.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/samsung/v2wifixx/full_v2wifixx.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_v2wifixx
+PRODUCT_NAME := bliss_v2wifixx
 PRODUCT_DEVICE := v2wifixx
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -11,3 +11,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=v2wifixx \
     PRODUCT_DEVICE=v2wifixx \
     TARGET_DEVICE=v2wifixx \
+	
